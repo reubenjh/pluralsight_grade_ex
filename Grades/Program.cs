4d0bb2d86fdc1c43045fd743a496a2b7dce3834d
reubenjh <rjharcourt@gmail.com>
2018-08-28 17:09:17 +1200

    added speech synth assembly

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Speech.Synthesis;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,14 +11,20 @@ namespace Grades
     {
         static void Main(string[] args)
         {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("Hello, this is a grade book program");
+
             Gradebook book = new Gradebook();
             book.AddGrade(91f);
             book.AddGrade(89.5f);
             book.AddGrade(75f);
 
             GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine("Average grade: ");
             Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine("Highest grade: ");
             Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine("Lowest grade: ");
             Console.WriteLine(stats.LowestGrade);
         }
     }
